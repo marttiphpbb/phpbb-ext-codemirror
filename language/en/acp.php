82c17fcd134aaa46827d6f316ddfb32a6aa7897f
martti <info@martti.be>
2018-10-21 08:25:13 +0200

    lang

--- language/en/acp.php
+++ language/en/acp.php
@@ -19,29 +19,41 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, [
 
 	'ACP_MARTTIPHPBB_CODEMIRROR_CONFIG_EXPLAIN'
-		=> 'This extension provides a basic integration of CodeMirror for use
-			by other extensions. See %1$sCodeMirror configuration%2$s and
-			%3$sconfiguration options defined by this extension%4$s
-			for all possible options.',
-	'ACP_MARTTIPHPBB_CODEMIRROR_VERSION'			=> '%1$sCodeMirror%2$s version: %3$s',
-	'ACP_MARTTIPHPBB_CODEMIRROR_THEME'				=> 'Theme',
-	'ACP_MARTTIPHPBB_CODEMIRROR_TRY_THEME'			=> 'Try theme',
-	'ACP_MARTTIPHPBB_CODEMIRROR_TRY_THEME_EXPLAIN'	=> 'This is to try other themes. Only "theme" defined in the json configuration below will be saved.',
-	'ACP_MARTTIPHPBB_CODEMIRROR_MODE'				=> 'Mode',
-	'ACP_MARTTIPHPBB_CODEMIRROR_KEYMAP'				=> 'Key map',
-	'ACP_MARTTIPHPBB_CODEMIRROR_CONFIG_SAVED'		=> 'The configuration was saved.',
-	'ACP_MARTTIPHPBB_CODEMIRROR_INVALID_JSON'		=> 'The json configuration contains at least one error.',
-	'ACP_MARTTIPHPBB_CODEMIRROR_RESTORE_DEFAULTS'	=> 'Restore defaults',
+	=> 'This extension provides a basic integration of CodeMirror for use
+		by other extensions. See %1$sCodeMirror configuration%2$s and
+		%3$sconfiguration options defined by this extension%4$s
+		for all possible options.',
+	'ACP_MARTTIPHPBB_CODEMIRROR_VERSION'
+	=> '%1$sCodeMirror%2$s version: %3$s',
+	'ACP_MARTTIPHPBB_CODEMIRROR_THEME'
+	=> 'Theme',
+	'ACP_MARTTIPHPBB_CODEMIRROR_TRY_THEME'
+	=> 'Try theme',
+	'ACP_MARTTIPHPBB_CODEMIRROR_TRY_THEME_EXPLAIN'
+	=> 'This selector is just a tool to preview themes;
+		it`s setting is not part of the configuration and won`t be saved.
+		To change the default theme you can edit or add a "theme"
+		in the JSON configuration below.',
+	'ACP_MARTTIPHPBB_CODEMIRROR_MODE'
+	=> 'Mode',
+	'ACP_MARTTIPHPBB_CODEMIRROR_KEYMAP'
+	=> 'Key map',
+	'ACP_MARTTIPHPBB_CODEMIRROR_CONFIG_SAVED'
+	=> 'The configuration was saved.',
+	'ACP_MARTTIPHPBB_CODEMIRROR_INVALID_JSON'
+	=> 'The JSON configuration contains at least one error.',
+	'ACP_MARTTIPHPBB_CODEMIRROR_RESTORE_DEFAULTS'
+	=> 'Restore defaults',
 	'ACP_MARTTIPHPBB_CODEMIRROR_JSON_ERROR_DEPTH'
-		=> 'JSON error: Maximum stack depth exceeded',
+	=> 'JSON error: Maximum stack depth exceeded',
 	'ACP_MARTTIPHPBB_CODEMIRROR_JSON_ERROR_STATE_MISMATCH'
-		=> 'JSON error: Underflow or the modes mismatch',
+	=> 'JSON error: Underflow or the modes mismatch',
 	'ACP_MARTTIPHPBB_CODEMIRROR_JSON_ERROR_CTRL_CHAR'
-		=> 'JSON error: Unexpected control character found',
+	=> 'JSON error: Unexpected control character found',
 	'ACP_MARTTIPHPBB_CODEMIRROR_JSON_ERROR_SYNTAX'
-		=> 'JSON syntax error, malformed JSON',
+	=> 'JSON syntax error, malformed JSON',
 	'ACP_MARTTIPHPBB_CODEMIRROR_JSON_ERROR_UTF8'
-		=> 'JSON error. Malformed UTF-8 characters, possibly incorrectly encoded.',
+	=> 'JSON error. Malformed UTF-8 characters, possibly incorrectly encoded.',
 	'ACP_MARTTIPHPBB_CODEMIRROR_JSON_ERROR_UNKNOWN'
-		=> 'JSON error: unknown error.',
+	=> 'JSON error: unknown error.',
 ]);
