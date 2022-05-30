2fceebae3f6907c142cc26ef218b9f89f95e3774
martti <info@martti.be>
2022-05-30 20:49:27 +0200

    version 0.7.0

--- event/listener.php
+++ event/listener.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * phpBB Extension - marttiphpbb CodeMirror
-* @copyright (c) 2018 - 2020 marttiphpbb <info@martti.be>
+* @copyright (c) 2018 - 2022 marttiphpbb <info@martti.be>
 * @license GNU General Public License, version 2 (GPL-2.0)
 */
 
